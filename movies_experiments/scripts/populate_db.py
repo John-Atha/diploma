b350d2a2ad6d685a19719c47e5a87e0842b2bbff
Giannis Athanasiou <giannis.athanasiou@libramli.ai>
2022-10-08 12:46:21 +0300

    Add scripts for more metadata and fastRP embeddings

--- movies_experiments/scripts/populate_db.py
+++ movies_experiments/scripts/populate_db.py
@@ -8,7 +8,10 @@ parent_path = pathlib.Path(os.getcwd()).parent.absolute()
 sys.path.append(str(parent_path))
 
 from utils.general import read_csv, df_to_json
-from utils.movies_with_metadata import insert_movies, insert_movies_genres, insert_movies_production_countries
+from utils.movies_with_metadata import \
+    insert_movies, insert_movies_genres, \
+    insert_movies_production_countries, insert_movies_production_companies, \
+    insert_movies_spoken_languages, add_fastRP_embeddings
 
 graph = Graph(
     "bolt://localhost:7687",
@@ -16,13 +19,20 @@ graph = Graph(
 )
 
 MOVIES_LIMIT = 100
+SKIP_DATA_INSERT = False
+SKIP_EMBEDDINGS_INSERT = False
 
-movies_df = read_csv(
-    filename="movies_metadata",
-    parent_dir_name="movies_with_metadata",
-)
-movies_json = df_to_json(movies_df)
+if not SKIP_DATA_INSERT:
+    movies_df = read_csv(
+        filename="movies_metadata",
+        parent_dir_name="movies_with_metadata",
+    )
+    movies_json = df_to_json(movies_df)
 
-insert_movies(graph, movies_json, MOVIES_LIMIT)
-insert_movies_genres(graph, movies_json)
-insert_movies_production_countries(graph, movies_json)
+    insert_movies(graph, movies_json, MOVIES_LIMIT)
+    insert_movies_genres(graph, movies_json)
+    insert_movies_production_countries(graph, movies_json)
+    insert_movies_production_companies(graph, movies_json)
+    insert_movies_spoken_languages(graph, movies_json)
+if not SKIP_EMBEDDINGS_INSERT:
+    add_fastRP_embeddings(graph)
--- movies_experiments/utils/movies_with_metadata.py
+++ movies_experiments/utils/movies_with_metadata.py
@@ -1,6 +1,7 @@
 import json
 from py2neo import Graph, Node, Relationship
 from py2neo.bulk import merge_nodes
+import ast
 
 # main methods
 def insert_movies(graph: Graph, movies, limit: int):
@@ -51,29 +52,94 @@ def insert_movies_production_countries(graph: Graph, movies_json):
         )
     print("Created and attached movies production countries.")
 
+def insert_movies_production_companies(graph: Graph, movies_json):
+    create_helper_nodes(
+        graph,
+        movies_json,
+        feature_name="production_companies",
+        feature_key="name",
+        merge_key=("ProductionCompany", "name"),
+        labels={"ProductionCompany"},
+    )
+    movies = graph.nodes.match("Movie")
+    for movie in movies:
+        attach_helper_nodes(
+            graph,
+            movie,
+            relationship_type="PRODUCED_BY",
+            feature_name="production_companies",
+            node_label="ProductionCompany",
+        )
+
+def insert_movies_spoken_languages(graph: Graph, movies_json):
+    create_helper_nodes(
+        graph,
+        movies_json,
+        feature_name="spoken_languages",
+        feature_key="name",
+        merge_key=("Language", "name"),
+        labels={"Language"},
+    )
+    movies = graph.nodes.match("Movie")
+    for movie in movies:
+        attach_helper_nodes(
+            graph,
+            movie,
+            relationship_type="SPEAKING",
+            feature_name="spoken_languages",
+            node_label="Language",
+        )
+
+def add_fastRP_embeddings(graph: Graph):
+    # projection
+    print("Running fastRP projection")
+    graph.run("""
+        CALL gds.graph.project(
+            'fastRP_embedding',
+            ['Movie', 'Genre', 'ProductionCompany', 'ProductionCountry', 'Language'],
+            {
+                BELONGS_TO: {
+                    orientation: 'UNDIRECTED'
+                },
+                PRODUCED_BY: {
+                    orientation: 'UNDIRECTED'
+                },
+                PRODUCED_IN: {
+                    orientation: 'UNDIRECTED'
+                },
+                SPEAKING: {
+                    orientation: 'UNDIRECTED'
+                }
+            }
+        )
+    """)
+    # write the generated embeddings to the nodes
+    print("Writting generated embeddings")
+    graph.run("""
+        CALL gds.fastRP.write(
+            'fastRP_embedding',
+            {
+                embeddingDimension: 128,
+                writeProperty: 'fastrp-embedding'
+            }
+        )    
+    """)
+    # drop the projection
+    print("Dropping projection")
+    graph.run(f"""
+        CALL gds.graph.drop("fastRP_embedding")
+    """)
 
 # helpers
 def transform_json_feature(movie, feature_name):
     try:
-        obj = json.loads(movie[feature_name].replace("\'", "\""))
+        obj = ast.literal_eval(movie[feature_name])
+        # obj = json.loads(movie[feature_name].replace("\'", "\""))
         return obj
     except Exception as e:
-        print("error at parsing json string:", movie[feature_name])
+        print(f"error at parsing json string: {feature_name}", movie[feature_name])
         print(e)
         return []
-
-def attach_helper_nodes(
-    graph: Graph,
-    movie: Node,
-    relationship_type: str,
-    feature_name: str,
-    node_label: str,
-):
-    REL = Relationship.type(relationship_type)
-    objects = transform_json_feature(movie, feature_name)
-    for object in objects:
-        node = graph.nodes.match(node_label, name=object["name"]).first()
-        graph.merge(REL(movie, node))
 
 def create_helper_nodes(graph: Graph, movies_json, feature_name, feature_key, merge_key, labels):
     nodes = get_nodes_set(movies_json, feature_name, feature_key)
@@ -94,3 +160,16 @@ def get_nodes_set(movies_json, feature_name, feature_key):
     nodes_dict = { node[feature_key]: node for node in nodes_flattened }
     nodes_unique = nodes_dict.values()
     return nodes_unique
+
+def attach_helper_nodes(
+    graph: Graph,
+    movie: Node,
+    relationship_type: str,
+    feature_name: str,
+    node_label: str,
+):
+    REL = Relationship.type(relationship_type)
+    objects = transform_json_feature(movie, feature_name)
+    for object in objects:
+        node = graph.nodes.match(node_label, name=object["name"]).first()
+        graph.merge(REL(movie, node))
