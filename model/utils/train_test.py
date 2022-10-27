73f245e8d92381d12d7756542df48faf79a15438
John-Atha <giannisj3@gmail.com>
2022-10-27 23:21:09 +0300

    Add some plotting of the predicted vs the target ratings

--- model/utils/train_test.py
+++ model/utils/train_test.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 def train_test(model, epochs, train_data, test_data, val_data, logging_step, lr=0.01):
 
@@ -17,30 +18,47 @@ def train_test(model, epochs, train_data, test_data, val_data, logging_step, lr=
         weight = 1. if weight is None else weight[target].to(pred.dtype)
         return (weight * (pred - target.to(pred.dtype)).pow(2)).mean()
     
-    def train():
+    def train(log=False):
         model.train()
         optimizer.zero_grad()
         pred = model(train_data.x_dict, train_data.edge_index_dict,
                         train_data['user', 'movie'].edge_label_index)
         target = train_data['user', 'movie'].edge_label
+        
+        # if log:
+        #     plt.figure(epoch, figsize=(30, 10))
+        #     plt.plot(target.detach()[:200])
+        #     plt.plot(pred.detach()[:200])
+        #     plt.legend(["target", "predictions"])
+        #     plt.show()
+
         loss = weighted_mse_loss(pred, target, weight)
         loss.backward()
         optimizer.step()
         return float(loss)
 
     @torch.no_grad()
-    def test(data):
+    def test(data, log=False):
         model.eval()
         pred = model(data.x_dict, data.edge_index_dict,
                     data['user', 'movie'].edge_label_index)
         pred = pred.clamp(min=0, max=5)
         target = data['user', 'movie'].edge_label.float()
+
+        # if log:
+        #     print(type(pred), type(target))
+        #     plt.figure(epoch, figsize=(30, 10))
+        #     plt.plot(target[:200])
+        #     plt.plot(pred[:200])
+        #     plt.legend(["target", "predictions"])
+        #     plt.show()
+
         rmse = F.mse_loss(pred, target).sqrt()
         return float(rmse)
     
     losses = []
     for epoch in range(1, epochs+1):
-        loss = train()
+        loss = train(log=not(epoch%20))
         train_rmse = test(train_data)
         val_rmse = test(val_data)
         test_rmse = test(test_data)
