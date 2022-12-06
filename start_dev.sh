a213c7c95efd8f937300c57e4e441a7c2252a1e8
John-Atha <giannisj3@gmail.com>
2022-12-06 21:25:57 +0200

    Update tmux dev script

--- start_dev.sh
+++ start_dev.sh
@@ -3,8 +3,10 @@ tmux -2 new-session -d -s $SESSION
 
 tmux new-window -t $SESSION:0
 tmux splitw -h
-tmux send-keys  -t 0 "cd front-end && npm run start" Enter
-tmux send-keys -t 1 "cd back-end && npm run dev" Enter
+tmux splitw -h
+tmux send-keys -t 0 "cd front-end && npm run start" Enter
+tmux send-keys -t 1 "cd movies-api && npm run dev"  Enter
+tmux send-keys -t 2 "cd users-api && npm run dev"   Enter
 
 tmux select-window -t $SESSION:0
 tmux -2 attach-session -t $SESSION
